08afa45f842c1ef88a82909b8292d25ffed07b0d
alexhkurz <alexhkurz@gmail.com>
2020-09-17 17:54:18 -0700

    minor changes

--- src/Haskell/numbers3.hs
+++ src/Haskell/numbers3.hs
@@ -1,11 +1,14 @@
 -- an interpreter for abstract syntax in Haskell
 
 -- a language for arithmetic expressions
-data Exp = Num Int | Plus Exp Exp | Times Exp Exp 
+data Exp = Num Integer | Plus Exp Exp | Times Exp Exp 
 
-eval :: Exp -> Int
+eval :: Exp -> Integer
 eval (Num n) = n
-eval (Plus n m) = (eval n) + (eval m)
-eval (Times n m) = (eval n) * (eval m)
+-- the next two equations show two common naming conventions
+-- choose the one you like better
+eval (Plus e1 e2) = (eval e1) + (eval e2)
+eval (Times e e') = (eval e) * (eval e')
 -- eval (Times (Num 2) (Num 3))
 -- eval (Plus (Num 1) (Times (Num 2) (Num 3)))
+
